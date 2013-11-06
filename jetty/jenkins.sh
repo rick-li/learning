6a8fc174fa71723d8502636c81ff3e387e272bc9
Rick Li <racke1983cn@hotmail.com>
2013-11-06 16:16:08 +0800

    leverage jetty.sh

--- jetty/jenkins.sh
+++ jetty/jenkins.sh
@@ -1,27 +1,6 @@
-#!/bin/bash
-
-start() {
-  $JAVA_HOME/bin/java -jar $JETTY_HOME/start.jar -DSTOP.PORT=9093 -DSTOP.KEY=stop_jetty --ini=webapps/jenkins/start.d/jenkins.ini  webapps/jenkins/start.d/jetty.xml
-}
-
-
-stop() {
-  $JAVA_HOME/bin/java -jar $JETTY_HOME/start.jar -DSTOP.PORT=9093 -DSTOP.KEY=stop_jetty 
-}
-
-
-case "$1" in
-  start)
-	start
-	;;
-  stop)
-	stop
-	;;
-  restart)
-	stop;start;
-	;;
-  *)
-	echo $"Usage: $0 {start|stop|restart}"
-	exit 1
-esac
-exit 0
+export APP_HOME=$JETTY_HOME/webapps/jenkins
+export JETTY_CONF=$APP_HOME/start.d/jenkins.conf
+export JETTY_LOGS=$APP_HOME/start.d/logs
+export JETTY_ARGS=jetty.port=9090 jetty.secure.port=9443
+export JETTY_PID=$APP_HOME/start.d/jekins.pid
+$JETTY_HOME/bin/jetty.sh -d $1
